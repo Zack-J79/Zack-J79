18a0fc4231a72ae5ea12a9e20ec8afdce7e07165
Zack-J79 <zjjensen@outlook.com>
2024-03-11 10:53:45 -0400

    changed

---
+++ PayHrs.py
@@ -0,0 +1,11 @@
+# Enter Hours: 35
+# Enter Rate: 2.75
+# Pay: 96.25
+
+# Ask for user input
+hrs = float(input('Enter hours worked: '))
+rate = float(input('Enter rate per hour: '))
+# Calculate gross pay
+gross_pay = hrs * rate
+# Display the result
+print('Gross Pay:', gross_pay)
